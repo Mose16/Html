fe791b3602614a33c5c59dd1bff4205b4ce99a75
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-03-26 12:44:57 +1300

    Add files via upload

--- code/generator.py
+++ code/generator.py
@@ -15,21 +15,32 @@ def createFile(fileName, text):
     return
 
 def addPass(line):
-    try:
-        line.find("def")
-    except ValueError:
-        return line
+    if line.find("def ") != -1:
+        pass
+    elif line.find("class ") != -1:
+        pass
     else:
-        pos = line.find("(") + 1
-        line = line[:pos] + "passing variables: " + line[pos:]
         return line
+    
+    if line.find("()") == -1:
+        pos = line.find("(") + 1
+        line = line[:pos] + "Passing variables: " + line[pos:]
+    
+    return line    
+    
         
 def wordReplace(line):
     for word in WORDS:
-        if line.find(word) != -1:
-            pos = line.find(word)
-            
-            line = line[:pos] + PSEUDO[word] + line[pos + len(word) - 1:]
+        if word[:3] == " = ":
+            if line.find(word) != -1:
+                pos = line.find(word)
+                line = PSEUDO[word] + line[:pos] + " to: " + line[pos + len(word) - 1:]
+                
+        else:
+            if line.find(word) != -1:
+                pos = line.find(word)
+                
+                line = line[:pos] + PSEUDO[word] + line[pos + len(word) - 1:]
     return line
         
             
@@ -38,12 +49,14 @@ def wordReplace(line):
 ###---Variables---###
 PSEUDO = {
     "def ":"Define function called",
-    "class ":"Define class called"
+    "class ":"Define class called",
+    " = ":"Set variable ",
 }
 
 WORDS = [
     "def ",
-    "class "
+    "class ",
+    " = "
 ]
 
 
@@ -57,7 +70,7 @@ for line in lines:
     line = addPass(line)
     line = wordReplace(line)
     
-    newFile += "\n" + line
+    newFile += "" + line
 
 print(newFile)
 
